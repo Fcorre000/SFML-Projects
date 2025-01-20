47e887f9e69565ec96cadf0bebe5252ad2e5186c
Fernando Correa <fernandocorrea2014@gmail.com>
2025-01-20 11:32:45 -0600

    moving sprite finished

--- Moving_Sprite/src.cpp
+++ Moving_Sprite/src.cpp
@@ -1,23 +1,108 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <chrono>
+#include <string.h>
 using namespace std;
 
-//function to control the sprite using the WASD keys
-void handleSpriteMovement(double &x, double &y, const sf::FloatRect &spriteBounds, int windowLength, int windowHeight){
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && (y > 0)) {
-        y -= 8;
+class Animation{
+public:
+    Animation() = default;
+    Animation(int x, int y, int width, int height){
+        texture.loadFromFile("/Users/fortnitekorea/Desktop/SFML-Projects/Moving_Sprite/DS DSi - Dragon Quest 6 Realms of Revelation - Guard of Gallows Moor Gate.png");
+        for(int i = 0; i < nFrames; i++){
+            frames[i] = {x + i * width, y,width,height};
+        }
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && (y + spriteBounds.height < windowHeight) ) {
-        y += 8;
+
+    void ApplyToSprite(sf::Sprite& s){
+        s.setTexture(texture);
+        s.setTextureRect(frames[iFrame]);
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && (x > 0)) {
-        x -= 8;
+
+    void Update(float dt){
+        time += dt;
+        while(time >= holdTime){
+            time -= holdTime;
+            Advance();
+        }
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && (x + spriteBounds.width) < windowLength) {
-        x += 8;
+
+private:
+    void Advance(){
+        if (forward) {
+            if (++iFrame >= nFrames - 1) {
+                forward = false;
+            }
+        } else {
+            if (--iFrame <= 0) {
+                forward = true;
+            }
+        }
     }
-}
+private:
+    static constexpr int nFrames = 3;
+    static constexpr float holdTime = 0.1f;
+    sf::Texture texture;
+    sf::IntRect frames[nFrames];
+    int iFrame = 0;
+    float time = 0.0f;
+    bool forward = true; // Direction flag for back-and-forth animation
+};
+
+class Character{
+private:
+    enum class AnimationIndex{
+        WalkingUp,
+        WalkingDown,
+        WalkingLeft,
+        WalkingRight,
+        Count
+    };
+public:
+    Character(const sf::Vector2f& pos) 
+        : pos(pos)
+    {      
+        sprite.setTextureRect({0,0,33,32});
+        animations[int(AnimationIndex::WalkingDown)] = Animation(0, 96, 33,32);
+        animations[int(AnimationIndex::WalkingLeft)] = Animation(0, 32, 33,32);
+        animations[int(AnimationIndex::WalkingRight)] = Animation(0, 64, 33,32);
+        animations[int(AnimationIndex::WalkingUp)] = Animation(0, 0, 33,32);
+
+        sprite.setScale(3, 3);
+    }
+    void Draw(sf::RenderTarget& rt) const{      
+        rt.draw(sprite);
+    }
+    void SetDirection(const sf::Vector2f& dir){
+        vel = dir * speed;
+        if(dir.x > 0.0f){
+            curAnimation = AnimationIndex::WalkingRight;
+        }else if(dir.x < 0.0f){
+            curAnimation = AnimationIndex::WalkingLeft;
+        }else if(dir.y > 0.0f){
+            curAnimation = AnimationIndex::WalkingUp;
+        }else if(dir.y < 0){
+            curAnimation = AnimationIndex::WalkingDown;
+        }
+    }
+    void Update(float dt){
+        pos += vel * dt;
+        animations[int(curAnimation)].Update(dt);
+        animations[int(curAnimation)].ApplyToSprite(sprite);
+        sprite.setPosition(pos);
+    }
+
+
+private:
+    static constexpr float speed = 50.0f;
+    sf::Vector2f pos;
+    sf::Vector2f vel = {0.0f,0.0f};
+    sf::Sprite sprite;
+    Animation animations[int (AnimationIndex::Count)];
+    AnimationIndex curAnimation = AnimationIndex::WalkingUp;
+
+};
 
 int main()
 {
@@ -28,28 +113,16 @@ int main()
    
     
     //Creating the sprite
-    sf::Texture texture;
-    texture.loadFromFile("/Users/fortnitekorea/Desktop/SFML-Projects/Moving_Sprite/—Pngtree—character pixel art man_6178368.png");
-    sf::Sprite sprite(texture);//setup our sprite with a texture
-    sprite.setScale(.2, .2);//shrink the sprite to fit window
-    double x = (windowLength / 3);//to be adjusted if sprite size is changed
-    double y = (windowHeight / 3);//to be adjusted if sprite size is changed
-    sf::FloatRect spriteBounds = sprite.getGlobalBounds();// Get the sprite's size
+    Character player({100.0f, 100.0f});
+      
 
-
-    //object for timing/fps
-    sf::Clock clock; 
-    window.setFramerateLimit(60);
+    //timepoint for delta time measurement
+    auto tp =  chrono::steady_clock::now();
 
     // Start the game loop
     while (window.isOpen())
     {
-        //compute frame time
-        float currentTime = clock.restart().asSeconds();//returns time object and stored as a float
-        float fps = 1.0f/(currentTime);
-        cout << "fps: " << fps << "\n";
 
-        sprite.setPosition(x,y);//sets initial position of sprite
         // Process events
         sf::Event event;
         while (window.pollEvent(event))
@@ -60,13 +133,38 @@ int main()
                 window.close();
             }           
         }
-        handleSpriteMovement(x, y, spriteBounds, windowLength, windowHeight);//movement with WASD keys
+        //get dt
+        float dt;
+        {
+            const auto new_tp =  chrono::steady_clock::now();
+            dt = chrono::duration<float>(new_tp - tp).count();
+            tp = new_tp;
+        }
+    
+
         
+        //handle input
+        sf::Vector2f dir = {0.0f, 0.0f};
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
+            dir.y -= 8;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
+            dir.y += 8;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
+            dir.x -= 8;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
+            dir.x += 8;
+        }
+        player.SetDirection(dir);
+        //update model
+        player.Update(dt);
  
         //clearing and displaying everything continuously until we stop the program
         //order of things drawn matters
         window.clear();// Clear screen
-        window.draw(sprite);//draw sprite 
+        player.Draw(window);//draw sprite 
         window.display();// Update the window
     }
  
